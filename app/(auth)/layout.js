6ebd18dc65e9236b897107b3bf61bfa8e5fa609a
devbattery <devbattery@outlook.com>
2025-10-02 20:26:35 +0900

    Feat[Auth]: 사용자 로그아웃 추가

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,4 +1,5 @@
 import "../globals.css";
+import { logout } from "@/actions/auth-actions";
 
 export const metadata = {
   title: "Next Auth",
@@ -10,7 +11,7 @@ export default function AuthRootLayout({ children }) {
     <>
       <header id={"auth-header"}>
         <p>Welcome back!</p>
-        <form action="">
+        <form action={logout}>
           <button>Logout</button>
         </form>
       </header>
